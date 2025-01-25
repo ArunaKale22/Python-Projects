49df8fa8067f2d935d6c8555f8572da52983ce88
ArunaKale22 <yellayiaruna@gmail.com>
2025-01-25 17:30:28 +0530

    Updated my logic

--- 1. Snake-Water-Gun-Game/main.py
+++ 1. Snake-Water-Gun-Game/main.py
@@ -1,15 +1,18 @@
-import random
+
 '''
 1 for snake
 0 for gun
 -1 for water 
 '''
-
+import random
 computer = random.choice([-1, 0, 1])
 youStr = input("Enter your choice: ")
 youDict = {"s" : 1, "w" : -1, "g" : 0}
-reverseDict = {1: "Snake", -1 : "Water", 0 : "Gun"}
+reverseDict = {1 : "Snake", -1 : "Water", 0 : "Gun"}
 
+if youStr not in youDict:
+    print("Invalid input! Please enter 's' for Snake, 'w' for Water, or 'g' for Gun.")
+    exit()
 you = youDict[youStr]
 
 print (f"You chose {reverseDict[you]}\n Computer chose {reverseDict[computer]}")
@@ -36,5 +39,5 @@ else:
     elif(computer == 0 and you == 1):
         print("You lose!")
 
-    else:
-        print("Something went wrong")
+    # else:
+    #     print("Something went wrong")
